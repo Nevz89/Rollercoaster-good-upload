44400775049f914ecf8015c9b040ad15ff50b8c7
Nevz89 <sven.oijen@capgemini.com>
2017-06-19 09:05:56 +0200

    Rollercoaster arraylist fix

--- src/nl/capgemini/Coastertester.java
+++ src/nl/capgemini/Coastertester.java
@@ -1,9 +1,10 @@
+package nl.capgemini;
 import java.util.ArrayList;
 
 
 public class Coastertester {
 	public static void main(String [] args){
-		ArrayList list = new ArrayList();
+		ArrayList <Rollercoaster> list = new ArrayList <Rollercoaster> ();
 		Rollercoaster taron = new Rollercoaster("Taron" ,"Phantasialand", "Intamin",true, 29.0, 1320,false, 4, 117, false);
 		list.add(taron);
 		Rollercoaster elToro = new Rollercoaster("El Toro","Six Flags Great Adventure", "Intamin",false, 55.0, 1300,false, 2, 115, false);
@@ -16,6 +17,10 @@ public class Coastertester {
 		list.add(kingDaKa);
 		Rollercoaster baron1898 = new Rollercoaster("Baron 1898","Efteling", "Bolliger & Mabillard",true, 37.5, 501,false, 3, 90, true);
 		list.add(baron1898);
+		Rollercoaster RockNRollercoaster = new Rollercoaster("Rock N Rollercoaster", "Disney's Hollywood Studios & Walt Disney Studio's", "Vekoma", true, 26.3, 1037, false, 4, 90, true);
+		list.add(RockNRollercoaster);
+		Rollercoaster ElCondor = new Rollercoaster("El Condor", "Walibi Holland", "Vekoma", true, 31.0, 662, true, 3, 80, true);
+		list.add(ElCondor);
 		for(Object l : list){
 		((Rollercoaster) l).displayCoaster();
 		System.out.println("");
